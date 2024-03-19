ec0679cfec3d416a95f4bcf760ec422221821dc3
s0571958 <nina.burdorf@student.htw-berlin.de>
2024-03-19 06:11:14 +0100

    streamlit update

--- src/app.py
+++ src/app.py
@@ -4,4 +4,8 @@ import plotly.graph_objects as go
 #page_settings------------
 page_icon = ":wheel:"
 page_title = "Energy Consumption Estimator"
+layout = "centered"
 #-------------------------
+
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
+st.title(page_title + " " + page_icon)
