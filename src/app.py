940c3012043b76932e4fdc0eeecdd8bd855435ca
s0571958 <nina.burdorf@student.htw-berlin.de>
2024-03-19 06:03:59 +0100

    polished setup initial commit

---
+++ src/app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import plotly.graph_objects as go
+
+#page_settings------------
+page_icon = ":wheel:"
+page_title = "Energy Consumption Estimator"
+#-------------------------
